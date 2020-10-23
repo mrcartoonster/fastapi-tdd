2488c191504fbc141bd24f60029d2eb16c429402
Evan Baird <mrcartoonster@gmail.com>
2020-10-23 09:38:15 -0600

    📦 NEW: FAS-4 #comment finishing pytest setup #done

--- project/tests/conftest.py
+++ project/tests/conftest.py
@@ -7,7 +7,7 @@ from app.config import Settings, get_settings
 from starlette.testclient import TestClient
 
 
-def get_settings_ovveride():
+def get_settings_override():
     return Settings(
         testing=1,
         database_url=os.environ.get("DATABASE_TEST_URL"),
@@ -17,7 +17,7 @@ def get_settings_ovveride():
 @pytest.fixture(scope="module")
 def test_app():
     # set up
-    main.app.dependency_overrides[get_settings] = get_settings_ovveride
+    main.app.dependency_overrides[get_settings] = get_settings_override
     with TestClient(main.app) as test_client:
 
         # testing
