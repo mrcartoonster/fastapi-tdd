e0d654b7d972b0244c518688b83f778e9ec9bf38
Evan Baird <mrcartoonster@gmail.com>
2020-10-24 10:55:29 -0600

    📦 NEW: FAS-6 #time 30m #comment Adding get request all endpoint

--- project/app/api/summaries.py
+++ project/app/api/summaries.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # project/app/api/summaries.py
-
+from typing import List
 
 from app.api import crud
 from app.models.pydantic import SummaryPayloadSchema, SummaryResponseSchema
@@ -30,3 +30,8 @@ async def read_summary(id: int) -> SummarySchema:
         raise HTTPException(status_code=404, detail="Summary not found")
 
     return summary
+
+
+@router.get("/", response_model=List[SummarySchema])
+async def read_all_summaries() -> List[SummarySchema]:
+    return await crud.get_all()
